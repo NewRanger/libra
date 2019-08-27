2cd30801dd7a168fa5068ed7c05f11656e5e87fa
newRanger <ohsomeonesin@gmail.com>
2019-08-27 04:26:00 +0400

    [function]:  getArrayMedian

---
+++ index.js
@@ -0,0 +1,7 @@
+// @import: Library
+const libra = require("./lib");
+
+const testArray = [4, 12, 13, 10, 11, 9, 76, 10, 7, 37];
+
+const value = libra.getArrayMedian(testArray);
+console.log(value);
