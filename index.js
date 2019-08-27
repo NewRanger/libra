c7ead9c673a0c9ba188b487db30db39eeeae83fe
newRanger <ohsomeonesin@gmail.com>
2019-08-27 04:52:44 +0400

    [impruved]: getArrayMedian

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // @import: Library
 const libra = require("./lib");
 
-const testArray = [4, 12, 13, 10, 11, 9, 76, 10, 7, 37];
+const testArray = [3, 5, 10, 1, 25, 4, 17, 51, 37];
 
 const value = libra.getArrayMedian(testArray);
 console.log(value);
